a63420428c14ab4d4b1659bf1043a52a5b926408
Igor Arambasic <igor.arambasic@gmail.com>
2014-05-27 11:42:04 +0200

    Number of Airpors as input

--- AmadWebServ/top_airports/views.py
+++ AmadWebServ/top_airports/views.py
@@ -7,14 +7,20 @@ import pandas as pd
 import numpy as np
 
 def index(request):
-	html = "<html><body>\
+	html = "<html>\
+	<head><title>Top N</title></head>\
+	<body>\
 	Welcome to Amadeus Data Processing Unit.<br> <br> \
-	We have access to the total number of passengers for every airport (year 2013).<br> \
-	The airports are sorted by the number of arriving passengers.<br> \
+	2013 was great year for airport transport, but what do you think,  which airport had the highest number of passengers?.<br> \
+	Well, we have sorted the airports by the number of arriving passengers.<br> \
 	This list is huge, so please giveus a number of the top arriving airports you whish to see:<br> \
+	<form action=\"/top_airports_json/\" method=\"get\">\
+  	Airports number: <input type=\"text\" name=\"n\"><br>\
+  	<input type=\"submit\" value=\"Submit\">\
+	</form>	\
 	<br> <br> <br> <br> \
 	The output will be given in JSON format<br> \
-	You can validate the output at: http://jsonformatter.curiousconcept.com/<br> \
+	You can validate the output at: <a href=\"http://jsonformatter.curiousconcept.com\" > http://jsonformatter.curiousconcept.com </a> \
 	</body></html>"
 	return HttpResponse(html)
 	
--- AmadWebServ/top_airports_json/views.py
+++ AmadWebServ/top_airports_json/views.py
@@ -7,7 +7,9 @@ import pandas as pd
 import numpy as np
 
 def top_airports_json(request):
-	n=10
+	n=int(request.GET.get('n', '10'))
+	if n<1 :
+		n=10
 	#BookFile='../../Data/BookingsNoDuplicates.csv'
 	BookFile='../../Data/bookings.csv'
 	BookingsAll = pd.read_csv(BookFile, sep='^', nrows=100, usecols=['arr_port','pax'])
@@ -17,8 +19,8 @@ def top_airports_json(request):
 	TopAirport=TopAirport.reset_index()
 	TopN=TopAirport[:n]
 	TopN.index=TopN.index+1
-	jsonTopN=TopN.to_json()
-	print >> open('WebOutputCheck.txt', 'w'), TopN , TopN, jsonTopN
+	jsonTopN=TopN.to_json(orient='records')
+	print >> open('WebOutputCheck.txt', 'w'), TopN , jsonTopN
     	#return HttpResponse(TopN)
 	#return HttpResponse("Hello, world. You're at the Airport index numero 2.")
 	return HttpResponse(jsonTopN)
